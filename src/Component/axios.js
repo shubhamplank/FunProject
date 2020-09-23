cdff22e6e033d13f6c2109ff3283dd5dc6cc9ff0
shubham.agarwal <shubham.plank@gmail.comgit config --global user.name  shubhamplank>
2020-09-23 14:10:06 +0530

    linting

--- src/Component/axios.js
+++ src/Component/axios.js
@@ -1,35 +1,26 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-
-
+import React, { Component } from "react";
+import axios from "axios";
 
 class Data extends Component {
-    constructor(prpos) {
-        super(prpos);
-        this.state = {
-            data: []
-        }
-
-    }
-
-    componentDidMount() {
-        const url = '/api/customers';  
-        axios.get(url).then(res => {this.setState({
-            data: res.data.data
-           });
-           console.log(res);
-        });     
-    }
-
-
-    render() {
-        console.log(this.state.data);
-        return  (
-            <div>
-                
-            </div>
-        )
-    }
+  constructor(prpos) {
+    super(prpos);
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    const url = "/api/customers";
+    axios.get(url).then(res => {
+      this.setState({
+        data: res.data.data
+      });
+      console.log(res);
+    });
+  }
+  render() {
+    console.log(this.state.data);
+    return <div></div>;
+  }
 }
 
 export default Data;
